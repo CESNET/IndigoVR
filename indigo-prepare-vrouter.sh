01fd242d10173452b850a2812baac2d74a0cb7d2
MP <puma@vsb.cz>
2017-05-05 13:52:54 +0200

    v2

--- indigo-prepare-vrouter.sh
+++ indigo-prepare-vrouter.sh
@@ -28,7 +28,7 @@ OPENVPNCFG="client.conf"				# OpenVPN config file
 LOCALNET4="eth1 10.11.12.1 10.11.12.0 255.255.255.0"	# LOCALNET4="interface router-IP network netmask"
 # --- /EDIT IT --------------
 
-PACKAGES="openvpn isc-dhcp-server"
+PACKAGES="openvpn"
 CNO=`tput sgr0`
 CRED=`tput setaf 1`
 CBOLD=`tput bold`
@@ -38,10 +38,7 @@ cat << EOF
 ${CBOLD}Indigo virtual router installation script${CNO}
 -----------------------------------------
 
-This script require Debian GNU/Linux or Ubuntu distribution. Script will
-install and configure these packages from official sources:
-
-	${CRED}${PACKAGES}${CNO}
+This script require Debian GNU/Linux or Ubuntu distribution.
 
 EOF
 
@@ -62,7 +59,6 @@ IP=`echo ${LOCALNET4} | awk '{print $2}'`
 NET=`echo ${LOCALNET4} | awk '{print $3}'`
 NETMASK=`echo ${LOCALNET4} | awk '{print $4}'`
 
-
 cat << EOF
 Current settings:
 
@@ -82,15 +78,38 @@ case "$A" in
 esac
 echo
 
+# DHCP confirmation ----------------------------------
+echo
+read -n1 -p "Do you want to install DHCP server? Some cloudes doesn't need it (for example: Amazon) [y/N] " DHCP
+echo
+case "$DHCP" in
+	y|Y)	
+		DHCP="yes"
+		PACKAGES="${PACKAGES} isc-dhcp-server"
+		echo "The DHCP server ${CRED}will be${CNO} installed";;
+	*)
+		DHCP="no"
+		echo "The DHCP server ${CRED}will NOT be${CNO} installed";;
+esac
+
 # Installing packages -------------------------------
-echo "Installing packages: ${PACKAGES}"
+
+cat << EOF
+
+Script will install and configure these packages from official sources:
+	${CRED}${PACKAGES}${CNO}
+
+EOF
+
+exit
 apt-get update && apt-get install ${PACKAGES}
 
 # Enabling IPv4 forwarding --------------------------
 echo
 echo "Enabling IPv4 forwarding..."
 sysctl net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/90-ivr.conf
+
 
 # Preparing OpenVPN config file ---------------------
 echo
@@ -104,11 +123,45 @@ echo "	Enabling OpenVPN service..."
 OSERVICE="`echo ${OPENVPNCFG} | sed 's/.conf$//'`"
 systemctl enable openvpn@${OSERVICE}
 
+
+# Configuring network --------------------------------
+echo
+echo "Configuring /etc/network/interfaces ..."
+echo "	Creating /etc/network/interface.backup"
+cp /etc/network/interfaces /etc/network/interfaces.backup
+echo "	Adding interface ${INTERFACE} ${IP}/${NETMASK} to /etc/network/interfaces"
+cat << EOF >> /etc/network/interfaces
+auto ${INTERFACE}
+iface ${INTERFACE} inet static
+	address ${IP}
+	netmask ${NETMASK}
+
+EOF
+/etc/init.d/networking restart
+
+
+# Configuring ISC DHCP server ------------------------
+if [ "$DHCP" = "yes" ]; then
+	echo
+	echo "Configuring ISC DHCP server..."
+	echo "	Creating /etc/dhcp/dhcpd.conf.backup"
+	cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
+	NET4="`echo ${IP} | cut -d. -f1-3`"
+	echo "INTERFACES=${INTERFACE}" >> /etc/default/isc-dhcp-server
+	cat << EOF >> /etc/dhcp/dhcpd.conf
+
+subnet ${NET4}.0 netmask ${NETMASK} { option routers ${IP}; allow unknown-clients; range ${NET4}.16 ${NET4}.254; }
+
+EOF
+	echo "	Restarting DHCP server..."
+	/etc/init.d/isc-dhcp-server restart
+fi
+
+
 # User confirmation ----------------------------------
 echo
 echo "Do you want to connect over VPN?"
-echo "${CRED}Warning: you can lost this connection!${CNO}"
-read -n1 -p "[y/N] " A 
+read -n1 -p "${CRED}Warning: you can lost this connection!${CNO} [y/N] " A 
 case "$A" in
 	y|Y) systemctl start openvpn@${OSERVICE} ;;
 	*) ;;
@@ -121,30 +174,4 @@ ${CRED}systemctl start openvpn@${OSERVICE}${CNO}
 
 EOF
 
-
-# Configuring network --------------------------------
-echo
-echo "Configuring /etc/network/interfaces ..."
-echo "	Creating /etc/network/interface.backup"
-cp /etc/network/interfaces /etc/network/interfaces.backup
-echo "	Adding interface ${INTERFACE} ${IP}/${NETMASK} to /etc/network/interfaces"
-cat << EOF >> /etc/network/interfaces
-auto ${INTERFACE}
-iface ${INTERFACE} inet static
-	address ${IP}
-	netmask ${NETMASK}'
-
-EOF
-
-# Configuring ISC DHCP server ------------------------
-echo
-echo "Configuring ISC DHCP server..."
-echo "	Creating /etc/dhcp/dhcpd.conf.backup"
-cp /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.backup
-NET4="`echo ${IP} | cut -d. -f1-3`"
-cat << EOF >> /etc/dhcp/dhcpd.conf
-
-subnet ${NET4}.0 netmask ${NETMASK} { option routers ${IP}; allow unknown-clients; range ${NET4}.16 ${NET4}.254; }
-
-EOF
 
